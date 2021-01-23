63b6f11a0def974f18482b4df48040b43f4e03df
99degree <>
2021-01-23 18:54:44 +0800

    Update replacement script.

--- updatefile.sh
+++ updatefile.sh
@@ -1,4 +1,7 @@
 echo copying...
 cd vendor
 cat  ../proprietary-files.txt | xargs -I %  cp % /mnt/amber_vendor/%
+chcon --reference=/mnt/amber_vendor/etc/NOTICE.xml.gz /mnt/amber_vendor/etc -R
+chcon --reference=/mnt/amber_vendor/firmware/wlan /mnt/amber_vendor/firmware -R
+chcon --reference=/mnt/amber_vendor/bin/grep /mnt/amber_vendor/bin/busybox
 echo done!
