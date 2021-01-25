0b159beeba7177f879fdfacba493caf4c4c3119e
99degree <>
2021-01-26 03:17:34 +0800

    Update camera_config.xml and duplicate audio_policy_configuration.xml to etc/

--- updatefile.sh
+++ updatefile.sh
@@ -4,4 +4,7 @@ cat  ../proprietary-files.txt | xargs -I %  cp % /mnt/amber_vendor/%
 chcon --reference=/mnt/amber_vendor/etc/NOTICE.xml.gz /mnt/amber_vendor/etc -R
 chcon --reference=/mnt/amber_vendor/firmware/wlan /mnt/amber_vendor/firmware -R
 chcon --reference=/mnt/amber_vendor/bin/grep /mnt/amber_vendor/bin/busybox
+chcon --reference=/mnt/amber_vendor/bin/init.class_main.sh /mnt/amber_vendor/bin/init.class_late.sh
+#u:object_r:qti_init_shell_exec:s0 
+#init.class_main.sh
 echo done!
